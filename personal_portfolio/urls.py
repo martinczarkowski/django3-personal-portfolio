d11a8eb9b15864cfa2231343080170e1fee4119b
Martin <martinczarkowski@gmail.com>
2020-06-08 14:53:14 +0200

    add a contact

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('generator/', generator_views.generator, name='generator'),
     path('password/', generator_views.password, name='password'),
+    path('contact/', views.contact, name='contact'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
